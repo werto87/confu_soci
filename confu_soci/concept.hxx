198415ceb20b6029ee98bb3664394b7e5a3529e7
werto87 <huhulasmichinruhe@web.de>
2021-06-10 09:58:57 +0200

    Update concept.hxx

--- confu_soci/concept.hxx
+++ confu_soci/concept.hxx
@@ -14,7 +14,8 @@
 #include <cstddef>
 #include <string>
 
-template <typename T> concept FusionSequence = requires (T a) { boost::fusion::at_c<0> (a); };
-template <typename T> concept IsOptional = requires (T a) { a.has_value (); };
-
+namespace confu_soci{
+  template <typename T> concept FusionSequence = requires (T a) { boost::fusion::at_c<0> (a); };
+  template <typename T> concept IsOptional = requires (T a) { a.has_value (); };
+}
 #endif /* A071053C_A9C4_4BFF_A10C_45887320F653 */
