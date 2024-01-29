a468d94bee59b160f6f1c7c3589589a93c6e7ee7
werto87 <huhulasmichinruhe@web.de>
2024-01-29 22:01:17 +0900

    new cmake layout

--- conanfile.py
+++ conanfile.py
@@ -6,16 +6,14 @@ class Project(ConanFile):
     generators = "CMakeToolchain", "CMakeDeps"
 
     def configure(self):
-        self.options["soci"]. with_sqlite3=True
-        self.options["soci"]. shared=False
-        self.options["soci"]. with_boost=True
-        self.options["boost"].header_only=True
+        self.options["soci"].with_sqlite3 = True
+        self.options["soci"].with_boost = True
         self.options["catch2"].with_main = True
         self.options["catch2"].with_benchmark = True
+        self.options["boost"].header_only = True
 
     def requirements(self):
         self.requires("soci/4.0.3")
         self.requires("catch2/2.13.7")
         self.requires("magic_enum/[>=0.9.5 <10]")
         self.requires("boost/1.83.0")
-
